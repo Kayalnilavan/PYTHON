a6f82300e58064674de297d634c6a9d2ffdf358e
Kayalnilavan <kayalnilavan110@gmail.com>
2026-02-11 06:08:49 +0530

    Add User Input file

---
+++ 09 FEB 2026/UserInput2.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter Number 1 : "))
+num2=int(input("Enter Number 2 : "))
+num_x=num1+num2
+print("Total 1 :",num_x)
+print("")
+num3=int(input("Enter Number 3 : "))
+num4=input("Enter Number 4 : ")
+num_y=num3+int(num4)
+print("Total 2 :",num_y)
