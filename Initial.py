ec74064d77cc647bdaff0fccd6825ee7244f0d3c
Kayalnilavan <kayalnilavan110@gmail.com>
2026-02-11 06:06:36 +0530

    Add Initial file

---
+++ Initial.py
@@ -0,0 +1,15 @@
+id=1001
+name="Kayal"
+age=22
+
+print("My id is",id)
+print("My name is",name)
+print("My age is",age)
+print("")
+print("My name is "+name)
+
+print("")
+print(type(id))  #finding data type
+print("")
+ids=type(id)
+print(ids)  #finding data type
