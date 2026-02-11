67d959dc407614a21d865727b356649da301c6c5
Kayalnilavan <kayalnilavan110@gmail.com>
2026-02-11 06:08:11 +0530

    Add First Exercise file

---
+++ Execise.py
@@ -0,0 +1,6 @@
+fname="Kayal"
+lname="Nilavan"
+email="kayalnilavan11@gmail.com"
+b_salary=150000
+
+print(f"*****************************************\n\tEmployee Details\n******************************************\nFull Name\t:{fname} {lname}\nEmail\t\t:{email}\nBasic Salary\t:Rs.{b_salary}\n******************************************")
