0c0a4c39456e22ccb0dca88910bc853d98bd940e
Kayalnilavan <kayalnilavan110@gmail.com>
2026-02-14 22:56:24 +0530

    Add Forloop_2.py demonstrating for-loops
    Add a new script that demonstrates basic for-loop usage: prints odd numbers from 1 to 9, outputs a blank line, then prints a countdown from 10 to 1. Includes a commented-out incorrect example (`for i in True:`) with a note explaining that range should be used rather than a boolean condition.

---
+++ 13 FEB 2026/Forloop_2.py
@@ -0,0 +1,11 @@
+for x in range(1,10,2):
+    print(x)
+print("")
+for y in range(10,0,-1):
+    print(y)
+    
+"""
+for i in True:
+    print(i)
+It can't use here.Here range used not condition
+"""
